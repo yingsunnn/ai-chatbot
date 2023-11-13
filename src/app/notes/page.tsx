a2423820c159c8b6ab4f517faa33e9a8ed0a0eaa
Ying <sco.suen@gmail.com>
2023-11-13 00:46:31 -0500

    note

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "ChatBot - Notes",
+}
+
+export default function NotesPage() {
+  return <div>here will be your notes</div>
+}
