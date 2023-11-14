57f7c815180b3d7721304df9cc7db5306c41e7fd
Ying <sco.suen@gmail.com>
2023-11-13 23:42:49 -0500

    feat: x

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,29 @@
-import { Metadata } from "next"
+import Note from "@/components/Note";
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "ChatBot - Notes",
-}
+};
 
-export default function NotesPage() {
-  return <div>here will be your notes</div>
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("userId undefined");
+
+  const allNotes = await prisma.note.findMany({ where: { userId } });
+
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full text-center">
+          {"You don't have any notes yet."}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatBot - Sign Out"
+  title: "ChatBot - Sign Up"
 }
 
 export default function SignUpPage() {
