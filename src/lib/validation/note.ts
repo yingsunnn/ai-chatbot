a08e9f1e5365647e22a418a5812eccfcd52ace62
Ying <sco.suen@gmail.com>
2023-11-17 19:46:06 -0500

    note

--- src/lib/validation/note.ts
+++ src/lib/validation/note.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const createNoteSchema = z.object({
   title: z.string().min(1, { message: "Title is required"}),
-  content: z.string().optional(),
+  content: z.string().min(1, { message: "Content is required"}),
 })
 
 export type CreateNoteSchema = z.infer<typeof createNoteSchema>;
